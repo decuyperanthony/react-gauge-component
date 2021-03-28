49473dc5e56a1426af1cd62a341b3d312f83f228
Anthony de Cuyper <decuyperanthony@gmail.com>
2021-03-28 14:05:07 +0200

    initial commit :tada:
    - use create react app
    - add reset css
    - add eslint && prettier

---
+++ src/Gauge/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const Gauge = ({ value = 50, min = 0, max = 100, label, units }) => {
+  return <div>Gauge</div>;
+};
+
+export default Gauge;
